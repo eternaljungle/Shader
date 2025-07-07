9b02fc6c2051cd2617c9952e3bfd2fe2eae121b7
yeonju <163484246+eternaljungle@users.noreply.github.com>
2025-07-07 12:03:05 +0900

    Update GPUGrassCommon.hlsl

--- GPUGrass/GPUGrassCommon.hlsl
+++ GPUGrass/GPUGrassCommon.hlsl
@@ -1,19 +1,47 @@
 #ifndef DEF_GPU_GRASS_COMMON
 #define DEF_GPU_GRASS_COMMON
 
-#define VERTEX_PER_BLADE 15                                                                        
-#define GRASS_BUFFER_SIZE 9                                                                        
-                                                                                                   
+#define GRASS_BUFFER_SIZE 9
+#define VERTEX_PER_BLADE 15     
+
+struct Grass
+{
+   float3 position;
+   float rotation;
+   float2 scale;
+   float bend;
+   uint2 colors;
+};
+
+RWByteAddressBuffer GrassBufferStore;
+void StoreGrass(Grass grass, uint id)
+{                               
+    GrassBufferStore.Store4(mad(id, GRASS_BUFFER_SIZE, 0) << 2, asuint(float4(grass.position, grass.rotation)));
+    GrassBufferStore.Store2(mad(id, GRASS_BUFFER_SIZE, 4) << 2, asuint(grass.scale));
+    GrassBufferStore.Store(mad(id, GRASS_BUFFER_SIZE, 6) << 2, asuint(grass.bend));
+    GrassBufferStore.Store2(mad(id, GRASS_BUFFER_SIZE, 7) << 2, asuint(grass.colors));  
+}
+
+ByteAddressBuffer GrassBufferLoad;
+Grass LoadGrass(uint id)
+{
+   Grass grass;
+   grass.position = asfloat(GrassBufferLoad.Load3(mad(id, GRASS_BUFFER_SIZE, 0) << 2));
+   grass.rotation = asfloat(GrassBufferLoad.Load(mad(id, GRASS_BUFFER_SIZE, 3) << 2));
+   grass.scale = asfloat(GrassBufferLoad.Load2(mad(id, GRASS_BUFFER_SIZE, 4) << 2));
+   grass.bend = asfloat(GrassBufferLoad.Load(mad(id, GRASS_BUFFER_SIZE, 6) << 2));
+   grass.colors = asuint(GrassBufferLoad.Load2(mad(id, GRASS_BUFFER_SIZE, 7) << 2));
+   return grass;
+}
+
 uint PackColor(float4 color)                                                                       
 {                                                                                                  
    const uint r = uint(saturate(color.x) * 255.0f) << 24;
    const uint g = uint(saturate(color.y) * 255.0f) << 16;
    const uint b = uint(saturate(color.z) * 255.0f) << 8;
    const uint a = uint(saturate(color.w) * 255.0f) << 0;
-   
    return uint(r | g | b | a);
 }                                                                                              
-   
 
 float4 UnPackColor(uint color)
 {
